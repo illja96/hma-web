7fff3c3109f06bbe223a06f33b11bc1514a9d1cc
Illia Khovanskyi <saskiuhia@gmail.com>
2020-05-15 18:24:57 +0300

    Add custom docker entry point file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleOAuthClientId: '$GOOGLE_OAUTH_CLIENT_ID$',
-  apiServerBaseUrl: '$API_SERVER_BASE_URL$'
+  googleOAuthClientId: 'GOOGLE_OAUTH_CLIENT_ID',
+  apiServerBaseUrl: 'API_SERVER_BASE_URL'
 };
