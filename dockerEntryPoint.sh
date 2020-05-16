bc7615a7edebb9a2f4f52faf22c54ccefc586b7a
Illia Khovanskyi <saskiuhia@gmail.com>
2020-05-16 19:32:40 +0300

    Fix dockerEntryPoint

--- dockerEntryPoint.sh
+++ dockerEntryPoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-set -xe : "${GOOGLE_OAUTH_CLIENT_ID?GOOGLE_OAUTH_CLIENT_ID argument was not provided}"
-set -xe : "${API_SERVER_BASE_URL?API_SERVER_BASE_URL argument was not provided}"
+set -xe
+: "${GOOGLE_OAUTH_CLIENT_ID?GOOGLE_OAUTH_CLIENT_ID argument was not provided}"
+: "${API_SERVER_BASE_URL?API_SERVER_BASE_URL argument was not provided}"
 
 sed -i "s/GOOGLE_OAUTH_CLIENT_ID/$GOOGLE_OAUTH_CLIENT_ID/g" /usr/share/nginx/html/main-*.js
 sed -i "s/API_SERVER_BASE_URL/$API_SERVER_BASE_URL/g" /usr/share/nginx/html/main-*.js
